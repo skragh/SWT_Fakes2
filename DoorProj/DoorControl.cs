d68f762063bf06d712b9506ea6ec1dbf08cc2949
Sigurd Skov Jensen <Sigurd.skov@hotmail.com>
2021-09-21 10:06:02 +0200

    DoorControl v1 done
    Not sure if actually done, but probably almost done?

---
+++ DoorProj/DoorControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorProj
+{
+    class DoorControl
+    {
+        IDoor _door;
+        IUserValidation _userValidation;
+        IEntryNotification _entryNotification;
+        IAlarm _alarm;
+        DoorControl(IDoor door, IUserValidation val, IEntryNotification en, IAlarm alarm)
+        {
+            _door = door;
+            _userValidation = val;
+            _entryNotification = en;
+            _alarm = alarm;
+        }
+
+        public void RequestEntryID(int id)
+        {
+            if (_userValidation.ValidateEntryRequest(id) == true)
+            {
+                _door.Open();
+                _entryNotification.NotifyEntryGranted(id);
+            }
+            else if(_userValidation.ValidateEntryRequest(id) == false)
+            {
+                _entryNotification.NotifyEntryDenied(id);
+            }
+            else
+            {
+                _alarm.RaiseAlarm();
+            }
+        }
+
+        public void DoorOpened()
+        {
+            _door.Close();
+        }
+
+        public void DoorClosed()
+        {
+            //Door has been closed, set alarm and stuff I guess..?
+        }
+
+    }
+}
