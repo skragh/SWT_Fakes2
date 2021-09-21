d540dc6f5f9d9646cb4aaacf60714d0a93f57847
skragh <83223908+skragh@users.noreply.github.com>
2021-09-21 09:52:52 +0200

    interfaces for subclasses

---
+++ DoorProj/Door.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorProj
+{
+    interface IDoor
+    {
+        public void Close();
+        public void Open();
+    }
+
+    interface IUserValidation
+    {
+        public bool ValidateEntryRequest(int id);
+    }
+
+    interface IEntryNotification
+    {
+        public void NotifyEntryGranted(int id);
+        public void NotifyEntryDenied(int id);
+    }
+
+    interface IAlarm
+    {
+        public void RaiseAlarm();
+    }
+}
